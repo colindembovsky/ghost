364f7eac5a76bea3aa123161e38a7cbc3ea3e265
Colin Dembovsky <colindembovsky@gmail.com>
2020-04-07 17:38:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var html_to_mobiledoc_1 = require("@tryghost/html-to-mobiledoc");
+html_to_mobiledoc_1["default"].toMobiledoc("<p>Hello</p>");
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Convert from Colin's ALM Corner MiniBlog to Ghost
+Repo for converting posts from MiniBlog to Ghost
