d1e590cdbb6bb9645e2cb4d63ac538d82d3eb101
Colin Dembovsky <colindembovsky@gmail.com>
2020-04-16 11:33:05 -0700

    Finalizing import

--- isso/run.sh
+++ isso/run.sh
@@ -3,6 +3,6 @@
 docker run \
     -d --name isso \
     -p 3002:8080 \
-    --mount type=bind,source="$(pwd)"/database,target=/isso/db \
-    --mount type=bind,source="$(pwd)"/config,target=/isso/config \
+    --mount type=bind,source="$(pwd)"/db/import,target=/db \
+    --mount type=bind,source="$(pwd)"/config/local,target=/config \
     wonderfall/isso:latest
