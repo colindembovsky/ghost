063ba09c605a670b794229bb06c09141ec9e3916
Colin Dembovsky <colindembovsky@gmail.com>
2020-04-16 21:12:19 -0700

    Updating isso and ghost SKU

--- infra/infra.sh
+++ infra/infra.sh
@@ -2,7 +2,7 @@
 
 # variables
 rg="cac-ghost"
-sku="S1"
+sku="B2"
 region="westus2"
 planName="cacghostplan"
 webAppName="cacghost"
@@ -47,19 +47,8 @@ az acr create -g $rg -n $acrName --sku Basic --admin-enabled true
 echo "Create storage account $saName"
 az storage account create -n $saName -g $rg -l $region --kind StorageV2 --sku Standard_LRS
 
-echo "Create Azure File storage containers"
-az storage share create -n $issoDbShareName --account-name $saName
-az storage share create -n $issoConfigShareName --account-name $saName
-
-echo "Uploading files to file share"
-key=$(az storage account keys list -g $rg -n $saName --query [0].value -o tsv)
-#az storage file upload --account-name $saName --account-key $key \ 
-#   -s $issoDbShareName --source ../docker/isso/config/empty/comments.db
-az storage file upload --account-name $saName --account-key $key \
-    -s $issoConfigShareName --source ../isso/config/azure/isso.conf
-
-echo "Creating MYSQL db"
-az mysql server create g $rg -n $mySQLServerName \
+echo "Creating MYSQL server $mySQLServerName"
+az mysql server create -g $rg -n $mySQLServerName \
     --admin-user $mySQLAdmin \
     --admin-password $mySQLPass --sku-name $mySQLSku \
     --ssl-enforcement Disabled \
@@ -71,17 +60,17 @@ az mysql server firewall-rule create --resource-group $rg --server $mySQLServerN
         --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
 echo "Creating Ghost Database"
-az mysql db create -g $rg -s mySQLServerName -n ghost
+az mysql db create -g $rg -s $mySQLServerName -n ghost
 
 echo "Creating app service plan $planName with sku $sku"
 az appservice plan create -g $rg -n $planName --sku $sku --is-linux
 
 echo "Creating webapp $webAppName with nginx image"
-az webapp create -g $rg -n $webAppName -p $planName -i nginx:latest
+az webapp create -g $rg -n $webAppName -p $planName -i "$acrName.azurecr.io/ghost:prod"
 
 echo "Setting url env setting"
 az webapp config appsettings set -g $rg -n $webAppName --settings \
-    --settings url=$ghostUrl
+    url=$ghostUrl \
     database__client=mysql \
     database__connection__database=ghost \
     database__connection__host=$mySQLServerName.mysql.database.azure.com \
@@ -109,18 +98,14 @@ az webapp create -g $rg -n $issoAppName -p $planName \
     --multicontainer-config-type "compose" \
     --multicontainer-config-file "../isso/isso-nginx.yml"
 
-echo "Creating file shares for isso"
-az webapp config storage-account add -g $rg -n $issoAppName --account-name $saName \
-    --custom-id isso-db --storage-type AzureFiles --access-key $key \
-    --share-name $issoDbShareName --mount-path "/db"
-
-az webapp config storage-account add -g $rg -n $issoAppName --account-name $saName \
-    --custom-id isso-conf --storage-type AzureFiles --access-key $key \
-    --share-name $issoConfigShareName --mount-path "/config"
+echo "Configure persistent storage"
+az webapp config appsettings set -g $rg -n $issoAppName --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
 
 echo "Setting registry for $issoAppName"
 az webapp config container set -g $rg -n $issoAppName \
-    --docker-registry-server-url "http://$acrName.azurecr.io" --docker-registry-server-user $acrName --docker-registry-server-password $acrPassword \
+    --docker-registry-server-url "http://$acrName.azurecr.io" \
+    --docker-registry-server-user $acrName \
+    --docker-registry-server-password $acrPassword \
     --multicontainer-config-type "compose" \
     --multicontainer-config-file "../isso/isso-nginx.yml"
 
@@ -131,13 +116,3 @@ az webapp log config -g $rg -n $issoAppName \
     --web-server-logging filesystem \
     --docker-container-logging filesystem \
     --level information
-
-# container images for dev
-# az acr login -n cacregistry
-# cd docker/ghost
-# docker build . -t cacregistry.azureacr.io/ghost:dev
-# docker push cacregistry.azureacr.io/ghost:dev
-# cd ../docker/ghost
-# docker build . -t cacregistry.azureacr.io/isso:dev
-# cd ../
-# docker push cacregistry.azureacr.io/isso:dev
