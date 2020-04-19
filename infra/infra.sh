c80dbe932a6c938d8289fda8c8bb7b9a2e62feff
Colin Dembovsky <colindembovsky@gmail.com>
2020-04-18 22:30:27 -0700

    Adding pipelines

--- infra/infra.sh
+++ infra/infra.sh
@@ -1,88 +1,67 @@
 #!/bin/bash
 
-# variables
-rg="cac-ghost"
-sku="B2"
-region="westus2"
-planName="cacghostplan"
-webAppName="cacghost"
-ghostDbShareName="cacghostdb"
-issoAppName="cacisso"
-slotName="test"
-saName="cacghoststore"
-issoDbShareName="cacissodb"
-issoConfigShareName="cacissoconfig"
-acrName="cacregistry"
-ghostUrl="https://$webAppName.azurewebsites.net"
-mySQLServerName="cacmysql"
-mySQLAdmin="admin_cac"
-mySQLPass=$1
-mySQLSku="B_Gen5_1"
-
 # for fish
-# set rg "cac-ghost"
-# set sku "S1"
-# set region "westus2"
-# set planName "cacghostplan"
-# set webAppName "cacghost"
-# set issoAppName "cacisso"
-# set slotName "test"
-# set saName "cacghoststore"
-# set issoDbShareName "cacissodb"
-# set issoConfigShareName "cacissoconfig"
-# set acrName "cacregistry"
-# set ghostUrl "https://$webAppName.azurewebsites.net"
-# set ghostUrl "https://$webAppName.azurewebsites.net"
-# set mySQLServerName "cacmysql"
-# set mySQLAdmin "admin_cac"
-# set mySQLPass "SomeL0ngP@ssw0rd"
-# set mySQLSku "B_Gen5_1"
+# set -x RG "cac-ghost"
+# set -x SKU "B2"
+# set -x REGION "westus2"
+# set -x PLAN_NAME "cacghostplan"
+# set -x GHOST_WEBAPP_NAME "cacghost"
+# set -x ISSO_WEBAPP_NAME "cacisso"
+# set -x GHOST_SLOT_NAME "test"
+# set -x SA_NAME "cacghoststore"
+# set -x ACR_NAME "cacregistry"
+# set -x GHOST_CDN "https://$GHOST_WEBAPP_NAME.azurewebsites.net"
+# set -x ISSO_CDN "https://$ISSO_WEBAPP_NAME.azurewebsites.net"
+# set -x MYSQL_SERVER_NAME "cacmysql"
+# set -x MYSQL_ADMIN "admin_cac"
+# set -x MYSQL_PASS "SomeL0ngP@ssw0rd"
+# set -x MYSQL_SKU "B_Gen5_1"
 
-echo "Creating resource group $rg in region $region"
-az group create -n $rg -l $region
+echo "Creating resource group $RG in REGION $REGION"
+az group create -n $RG -l $REGION
 
 echo "Create container registry"
-az acr create -g $rg -n $acrName --sku Basic --admin-enabled true
+az acr create -g $RG -n $ACR_NAME --sku Basic --admin-enabled true
 
-echo "Create storage account $saName"
-az storage account create -n $saName -g $rg -l $region --kind StorageV2 --sku Standard_LRS
+echo "Create storage account $SA_NAME"
+az storage account create -n $SA_NAME -g $RG -l $REGION --kind StorageV2 --sku Standard_LRS
 
-echo "Creating MYSQL server $mySQLServerName"
-az mysql server create -g $rg -n $mySQLServerName \
-    --admin-user $mySQLAdmin \
-    --admin-password $mySQLPass --sku-name $mySQLSku \
+echo "Creating MYSQL server $MYSQL_SERVER_NAME"
+az mysql server create -g $RG -n $MYSQL_SERVER_NAME \
+    --admin-user $MYSQL_ADMIN \
+    --admin-password $MYSQL_PASS --sku-name $MYSQL_SKU \
     --ssl-enforcement Disabled \
-    --location $region
+    --location $REGION
 
 echo "Configure firewall for Azure services"
-az mysql server firewall-rule create --resource-group $rg --server $mySQLServerName \
+az mysql server firewall-rule create --resource-group $RG --server $MYSQL_SERVER_NAME \
         --name AllowAzureIP \
         --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
 echo "Creating ghost Database"
-az mysql db create -g $rg -s $mySQLServerName -n ghost
+az mysql db create -g $RG -s $MYSQL_SERVER_NAME -n ghost
 
 echo "Creating comments Database"
-az mysql db create -g $rg -s $mySQLServerName -n comments
+az mysql db create -g $RG -s $MYSQL_SERVER_NAME -n comments
 
-echo "Creating app service plan $planName with sku $sku"
-az appservice plan create -g $rg -n $planName --sku $sku --is-linux
+echo "Creating app service plan $PLAN_NAME with sku $SKU"
+az appservice plan create -g $RG -n $PLAN_NAME --sku $SKU --is-linux
 
-echo "Creating webapp $webAppName with nginx image"
-az webapp create -g $rg -n $webAppName -p $planName -i "$acrName.azurecr.io/ghost:prod"
+echo "Creating webapp $GHOST_WEBAPP_NAME with nginx image"
+az webapp create -g $RG -n $GHOST_WEBAPP_NAME -p $PLAN_NAME -i "$ACR_NAME.azurecr.io/ghost:prod"
 
 echo "Setting url env setting"
-az webapp config appsettings set -g $rg -n $webAppName --settings \
-    url=$ghostUrl \
+az webapp config appsettings set -g $RG -n $GHOST_WEBAPP_NAME --settings \
+    url=$GHOST_CDN \
     database__client=mysql \
     database__connection__database=ghost \
-    database__connection__host=$mySQLServerName.mysql.database.azure.com \
-    database__connection__user=$mySQLAdmin@$mySQLServerName \
-    database__connection__password=$mySQLPass \
+    database__connection__host=$MYSQL_SERVER_NAME.mysql.database.azure.com \
+    database__connection__user=$MYSQL_ADMIN@$MYSQL_SERVER_NAME \
+    database__connection__password=$MYSQL_PASS \
     WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
 
 echo "Enabling docker container logging"
-az webapp log config -g $rg -n $webAppName \
+az webapp log config -g $RG -n $GHOST_WEBAPP_NAME \
     --application-logging true \
     --detailed-error-messages true \
     --web-server-logging filesystem \
@@ -90,30 +69,30 @@ az webapp log config -g $rg -n $webAppName \
     --level information
 
 echo "Update settings to re-init container"
-az webapp config container set -g $rg -n $webAppName -i "$acrName.azurecr.io/ghost:prod"
+az webapp config container set -g $RG -n $GHOST_WEBAPP_NAME -i "$ACR_NAME.azurecr.io/ghost:prod"
 
-echo "Creating slot $slotName"
-az webapp deployment slot create -g $rg -n $webAppName -s $slotName
+echo "Creating slot $GHOST_SLOT_NAME"
+az webapp deployment slot create -g $RG -n $GHOST_WEBAPP_NAME -s $GHOST_SLOT_NAME
 
-echo "Creating webapp $issoAppName"
-acrPassword=$(az acr credential show -g $rg -n $acrName --query "[passwords[?name=='password'].value]" --output tsv)
-az webapp create -g $rg -n $issoAppName -p $planName \
+echo "Creating webapp $ISSO_WEBAPP_NAME"
+acrPassword=$(az acr credential show -g $RG -n $ACR_NAME --query "[passwords[?name=='password'].value]" --output tsv)
+az webapp create -g $RG -n $ISSO_WEBAPP_NAME -p $PLAN_NAME \
     --multicontainer-config-type "compose" \
     --multicontainer-config-file "../isso/isso-nginx.yml"
 
 echo "Configure persistent storage"
-az webapp config appsettings set -g $rg -n $issoAppName --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
+az webapp config appsettings set -g $RG -n $ISSO_WEBAPP_NAME --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
 
-echo "Setting registry for $issoAppName"
-az webapp config container set -g $rg -n $issoAppName \
-    --docker-registry-server-url "http://$acrName.azurecr.io" \
-    --docker-registry-server-user $acrName \
+echo "Setting registry for $ISSO_WEBAPP_NAME"
+az webapp config container set -g $RG -n $ISSO_WEBAPP_NAME \
+    --docker-registry-server-url "http://$ACR_NAME.azurecr.io" \
+    --docker-registry-server-user $ACR_NAME \
     --docker-registry-server-password $acrPassword \
     --multicontainer-config-type "compose" \
     --multicontainer-config-file "../isso/isso-nginx.yml"
 
 echo "Enabling docker container logging"
-az webapp log config -g $rg -n $issoAppName \
+az webapp log config -g $RG -n $ISSO_WEBAPP_NAME \
     --application-logging true \
     --detailed-error-messages true \
     --web-server-logging filesystem \
