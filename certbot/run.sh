0ecf396575dab4ecde71fdd5d463e72eed3f3499
Colin Dembovsky <colindembovsky@gmail.com>
2020-04-19 15:31:19 -0700

    Finalize certbot Dockerfile and run script Add certbot to nginx pipeline

---
+++ certbot/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+rsa_key_size=4096
+data_path="./data/certbot"
+if [ -z $STAGING ] || [ $STAGING != "0" ]; then staging_arg="--staging"; fi
+
+if [ -z $EMAIL ] || [ -z $CDN ]; then
+  echo "Please set email and CDN!"
+else
+    echo "Registering cert"
+    echo "Staging arg: $STAGING"
+    certbot certonly --webroot -w /var/www/certbot \
+        $staging_arg \
+        $EMAIL \
+        -d $CDN -d www.$CDN \
+        --rsa-key-size $rsa_key_size \
+        --agree-tos \
+        --force-renewal
+
+    # loop infinitely and check for cert renewal every 12 hours
+    trap exit TERM; while :; do certbot renew; sleep 10s & wait $!; done;
+fi
